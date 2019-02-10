35907bdbc2e4f38790584b5839fcf2fbb06ac47b
Gabe Hargrave <gabe.hargrave@vacasa.com>
2019-02-10 09:56:57 -0800

    Tweaks some settings for upgraded libs

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -13,9 +13,6 @@ if (!initialized) {
   // firebase.initializeApp(isDevEnv() ? devConfig : prodConfig)
   firebase.initializeApp(devConfig)
   firebaseAuth = firebase.auth()
-  const firestore = firebase.firestore()
-  const settings = { timestampsInSnapshots: true }
-  firestore.settings(settings)
   db = firebase.firestore()
   initialized = true
 }
