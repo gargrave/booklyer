e82f235743cfa85c801b1e2a39d02bdf2378023c
Gabe Hargrave <gkhargrave@gmail.com>
2019-02-11 11:12:50 -0800

    Another attempt at Travis

---
+++ scripts/ci/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  export FIREBASE_PROJECT=prod
+else
+  export FIREBASE_PROJECT=default
+fi
+yarn firebase use $FIREBASE_PROJECT --token=$FIREBASE_CI_TOKEN
+yarn firebase deploy --token=$FIREBASE_CI_TOKEN
