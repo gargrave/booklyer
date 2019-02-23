751e27ec0a33f8cef75e765ace6066f2fff2ffc1
Gabe Hargrave <gkhargrave@gmail.com>
2019-02-23 12:23:12 -0800

    Mock data and TSLint expansions

---
+++ src/utils/mocks/static/users.ts
@@ -0,0 +1,14 @@
+import { User } from 'app/auth/auth.types'
+
+const mockUsers: User[] = [
+  {
+    creationTime: 'Mon, 03 Dec 2018 00:10:05 GMT',
+    displayName: 'Billy Pickles',
+    email: 'billypickles@gmail.com',
+    emailVerified: true,
+    id: 'Ik76ZQAKfffXvaHIIAwAKnfi1O52',
+    lastSignInTime: 'Sat, 23 Feb 2019 18:45:08 GMT',
+  },
+]
+
+export { mockUsers }
