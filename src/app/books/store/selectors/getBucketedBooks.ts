f424cf4697099e6433394db0bd2d75dda00d1269
Gabe Hargrave <gkhargrave@gmail.com>
2019-07-28 11:38:40 -0700

    Fix book sorting to to mix title/sortBy
    (Rather than pushing "sortBy" books to a separate sorting
    bucket at the end)

--- src/app/books/store/selectors/getBucketedBooks.ts
+++ src/app/books/store/selectors/getBucketedBooks.ts
@@ -3,15 +3,7 @@ import { createSelector } from 'reselect'
 import { bucketizer } from 'utils/bucketizer'
 import { Book } from '../../books.types'
 import { getBooks } from './getBooks'
-import { sanitizeTitle } from './utils'
-
-type BookSubBucket = {
-  sorted: Book[]
-  unsorted: Book[]
-}
-
-const sortByField = (field: string) => (a: Book, b: Book) =>
-  sanitizeTitle(a[field]) > sanitizeTitle(b[field]) ? 1 : -1
+import { sortByCustomOrTitle } from './utils'
 
 const sortByAuthor = (a: Book, b: Book) =>
   a.author.lastName > b.author.lastName ? 1 : -1
@@ -23,25 +15,9 @@ export function bookBucketizer(
 ) {
   // use default bucketizer to get single-level bucketing
   const bucketed = bucketizer<Book>(values, preSortValues, getBucketKey)
-
   // expand bucketing to account for "sortBy" field on books, as these books should be sorted relative to one-another
-  bucketed.forEach((bucket, idx) => {
-    const subBuckets = bucket.values.reduce(
-      (acc, book): BookSubBucket => {
-        const key = !!book.sortBy ? 'sorted' : 'unsorted'
-        return {
-          ...acc,
-          [key]: acc[key].concat(book),
-        }
-      },
-      { sorted: [] as Book[], unsorted: [] as Book[] },
-    )
-
-    // just directly overwrite the existing values on this bucket
-    bucketed[idx].values = [
-      ...subBuckets.unsorted.sort(sortByField('title')),
-      ...subBuckets.sorted.sort(sortByField('sortBy')),
-    ]
+  bucketed.forEach(({ values }) => {
+    values.sort(sortByCustomOrTitle)
   })
   return bucketed
 }
